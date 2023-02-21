ca6b5fcf9ae3e83b914dfeb0c990cdc45d96da3c
sohail70 <sohail.e.nia@gmail.com>
2023-02-21 20:37:17 +0330

    Finish aa_my_pack_cpp

--- aa_my_pack_cpp/src/helloWorld.cpp
+++ aa_my_pack_cpp/src/helloWorld.cpp
@@ -2,5 +2,10 @@
 
 int main(int argc , char* argv[])
 {
+    rclcpp::init(argc , argv);
+    auto node = rclcpp::Node::make_shared("Hi");
+    RCLCPP_INFO(node->get_logger() , "Hi my name is soheil");
 
+    rclcpp::shutdown();
+    return 0;
 }
