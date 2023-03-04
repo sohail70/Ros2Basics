3eee9b181a9ec65f7c64ab3f2588c9743e74a923
sohail70 <sohail.e.nia@gmail.com>
2023-03-04 17:06:24 +0330

    Add obstacle avoidance node in cpp

--- af_obstacle_avoidance_cpp/src/avoidance.cpp
+++ af_obstacle_avoidance_cpp/src/avoidance.cpp
@@ -1,3 +1,8 @@
+// for debugging use --> https://gist.github.com/JADC362/a4425c2d05cdaadaaa71b697b674425f
+/*
+ros2 run --prefix 'gdbserver localhost:3000' af_obstacle_avoidance_cpp avoidance  --> use this to create a gdbserver 
+and then use the play button in the debugger section after you created the launch.json for vs code
+*/
 #include<rclcpp/rclcpp.hpp>
 #include<geometry_msgs/msg/twist.hpp>
 #include<sensor_msgs/msg/laser_scan.hpp>
@@ -15,12 +20,14 @@ class Avoidance : public rclcpp::Node
         qos_profile.best_effort();
         sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>("scan" , qos_profile , std::bind(&Avoidance::sensorCallback , this ,std::placeholders::_1 ));
         timer_ = this->create_wall_timer(500ms , std::bind(&Avoidance::timerCallback,this));
+        vel_ = std::make_shared<geometry_msgs::msg::Twist>(); // If you forget to initialize then you process will die when you launch the launch file!
+        scan_ = std::make_shared<sensor_msgs::msg::LaserScan>();
     }
 
     private:
         void timerCallback()
         {
-
+            pub_->publish(*vel_); //if you don't initialize velocity in the constructur you'll recieve this error-->failed to publish message: ros_message argument is null
         }
         void sensorCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
         {
@@ -32,7 +39,31 @@ class Avoidance : public rclcpp::Node
 
         void process()
         {
-         
+            if(scan_->ranges[0] > 1.0)
+            {
+                vel_->linear.x = 0.2;
+                vel_->angular.z = 0.0;
+            }
+            if(scan_->ranges[0] < 1.0)
+            {
+                vel_->linear.x = 0.05;
+                vel_->angular.z = 0.1;
+            }    
+            if(scan_->ranges[270] < 1.0)
+            {
+                vel_->linear.x = 0.05;
+                vel_->angular.z = 0.1;
+            }
+            if (scan_->ranges[90] < 1.0)
+            {
+                vel_->linear.x = 0.05;
+                vel_->angular.z = -0.1;
+            }
+            if (scan_->ranges[0] <0.3)
+            {
+                vel_->linear.x = 0.0;
+                vel_->angular.z = 0.2;
+            }
         }
 
         geometry_msgs::msg::Twist::SharedPtr vel_;
@@ -51,4 +82,7 @@ int main(int argc , char* argv[])
     auto node = std::make_shared<Avoidance>();
     rclcpp::spin(node);
 
+    rclcpp::shutdown(); 
+    return 0;
 }
+
