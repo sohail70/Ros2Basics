20dcf129b1ab6dc494de40ed36872358a595d42b
sohail70 <sohail.e.nia@gmail.com>
2023-03-05 18:51:36 +0330

    Add functionality

--- af_obstacle_avoidance_py/af_obstacle_avoidance_py/avoidance.py
+++ af_obstacle_avoidance_py/af_obstacle_avoidance_py/avoidance.py
@@ -1,14 +1,44 @@
 import rclpy
 from rclpy.node import Node
-
-
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from rclpy.qos import QoSProfile , ReliabilityPolicy
 
 
 class Avoidance(Node):
     def __init__(self):
         super().__init__("Avoidance_node")
-        self.get_logger().error("HELLO")
+        self.pub_ = self.create_publisher(Twist , "cmd_vel" , 10)
+        qos_profile = QoSProfile(reliability = ReliabilityPolicy.BEST_EFFORT , depth=10) # you can look up why I wrote reliability and depth inside QosProfile class 
+        self.sub_ = self.create_subscription(LaserScan , "scan" , self.scan_callback ,qos_profile = qos_profile )
+        self.timer_ = self.create_timer(0.2 , self.timer_callback)
+        self.scan_= LaserScan()
+        self.vel_ = Twist()
+    
+    def timer_callback(self):
+        self.pub_.publish(self.vel_)
 
+    def scan_callback(self , msg):
+        self.scan_ = msg
+        # self.get_logger().info("%s" %str(self.scan_.ranges[0]))
+        self.process() 
+
+    def process(self):
+        if self.scan_.ranges[0] > 1.0:
+            self.vel_.linear.x = 0.2
+            self.vel_.angular.z = 0.0
+        if self.scan_.ranges[0] < 1.0:
+            self.vel_.linear.x = 0.05
+            self.vel_.angular.z = 0.1
+        if self.scan_.ranges[270] < 1.0:
+            self.vel_.linear.x = 0.05
+            self.vel_.angular.z = 0.1
+        if self.scan_.ranges[90] < 1.0:
+            self.vel_.linear.x = 0.05
+            self.vel_.angular.z = -0.1
+        if self.scan_.ranges[0] < 0.3:
+            self.vel_.linear.x = 0.0
+            self.vel_.angular.z = 0.2
 
 
 def main(args=None):
--- af_obstacle_avoidance_py/launch/avoidance.launch.py
+++ af_obstacle_avoidance_py/launch/avoidance.launch.py
@@ -3,8 +3,9 @@ from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription , DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python import get_package_share_directory
+from launch.substitutions import Command
 import os
-
+import random
 
 
 
@@ -17,11 +18,19 @@ def generate_launch_description():
 
 
     ## Robot State Publisher
-
+    robot_state_publisher = Node(package="robot_state_publisher" , executable="robot_state_publisher" , name="robot_state_publisher" , output="screen" ,
+                                parameters=[{'robot_description': Command(['xacro ', os.path.join(get_package_share_directory("af_obstacle_avoidance_py"),"model","my_tb3.urdf.xacro")   ]   )}]    
+                                )
 
 
     ## Spawn 
-
+    spawn_robot = Node(package="gazebo_ros" , executable="spawn_entity.py" , name="spawn_entity" , output="screen" , 
+        arguments= [
+            "-entity" , "tb3"+str(int(random.random()*10000)) ,
+             "-topic" , "robot_description",
+             '-x' , str(0.0) , '-y' , str(0.0) , '-z' , str(0.0),
+             '-R' , str(0.0) , '-P' , str(0.0) , '-Y' , str(0.0),
+            ])
 
     ## My nodes
     avoidance = Node(package="af_obstacle_avoidance_py" , executable="avoidance" , name="avoidance" , output="screen")
@@ -29,6 +38,8 @@ def generate_launch_description():
     ld = LaunchDescription()
     ld.add_action(args)
     ld.add_action(gazebo)
+    ld.add_action(robot_state_publisher)
+    ld.add_action(spawn_robot)
     ld.add_action(avoidance)
 
     return ld
