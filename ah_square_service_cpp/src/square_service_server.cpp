25e9435a8faf9bf9601bf9e7ebcf63bd75f3cd09
sohail70 <sohail.e.nia@gmail.com>
2023-03-12 16:12:42 +0330

    Add Sqaure Service Server with Custom Service Message

--- ah_square_service_cpp/src/square_service_server.cpp
+++ ah_square_service_cpp/src/square_service_server.cpp
@@ -1,6 +1,10 @@
 #include<rclcpp/rclcpp.hpp>
 #include<geometry_msgs/msg/twist.hpp>
 #include<ah_square_service_cpp/srv/my_custom_service_message.hpp>
+#include<chrono>
+
+
+// Call it by using --> ros2 service call /square_server ah_square_service_cpp/srv/MyCustomServiceMessage "{distance: 0.1 , repititions: 2}" 
 
 using  CustomReqPtr = ah_square_service_cpp::srv::MyCustomServiceMessage::Request::SharedPtr;
 using  CustomResPtr = ah_square_service_cpp::srv::MyCustomServiceMessage::Response::SharedPtr;
@@ -12,9 +16,15 @@ class SquareServer : public rclcpp::Node
         {
             square_service_ = this->create_service<ah_square_service_cpp::srv::MyCustomServiceMessage>("square_server" , std::bind(&SquareServer::serviceServer ,this ,std::placeholders::_1 , std::placeholders::_2));
             pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel" , 10);
-            vel_lin_.linear.x = 0.1;
-            vel_lin_.angular.z = 0.05;
             
+            vel_lin_.linear.x = 0.2;
+            vel_lin_.angular.z = 0.0;
+            
+            vel_ang_.linear.x = 0.0;
+            vel_ang_.angular.z = 0.1;
+
+            vel_stop_.linear.x = 0.0;
+            vel_stop_.angular.z = 0.0;
             
         }
 
@@ -22,15 +32,34 @@ class SquareServer : public rclcpp::Node
         void serviceServer(const CustomReqPtr req , const CustomResPtr res)
         {
             RCLCPP_INFO(this->get_logger() , "Service has been called");
+            double distance = req->distance;
+            int repetitions = req->repititions;
+            double time_needed;
 
-            // double time_needed;
-            // for(int i = 0 ; i< reps ; i++)
-            // {
-            //     for (int j = 0 ; j <4 ; j++)
-            //     {
-            //     }
-            // }
-              
+            for (int i = 0 ; i <repetitions ; i++)
+            {
+                for (int j =  0 ; j < 4 ; j ++)
+                {
+                    // Staright
+                    time_needed = distance / vel_lin_.linear.x ; 
+                    auto sleep_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<double>(time_needed));
+                    RCLCPP_INFO(this->get_logger(), "Nano Seconds: %s" , std::to_string(sleep_duration.count()).c_str());
+                    pub_->publish(vel_lin_);
+                    rclcpp::sleep_for(sleep_duration);
+                    // Stop
+                    pub_->publish(vel_stop_);
+                    // Turn
+                    time_needed = M_PI_2/vel_ang_.angular.z;
+                    pub_->publish(vel_ang_);
+                    sleep_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<double>(time_needed));
+                    rclcpp::sleep_for(sleep_duration);
+                    // Stop
+                    pub_->publish(vel_stop_);
+                }
+            }
+
+            res->success = true;
+
         }
 
 
