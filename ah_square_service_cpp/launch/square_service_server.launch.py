a13d8a7a5b08bd5ecca17c577714a395bc9780a9
sohail70 <sohail.e.nia@gmail.com>
2023-03-12 15:11:09 +0330

    Add custom service message

--- ah_square_service_cpp/launch/square_service_server.launch.py
+++ ah_square_service_cpp/launch/square_service_server.launch.py
@@ -8,7 +8,7 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command
 from ament_index_python import get_package_share_directory
 import os 
-
+import random
 
 def generate_launch_description():
     ## Gazebo
@@ -16,25 +16,33 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource( os.path.join(get_package_share_directory("gazebo_ros") ,"launch" , "gazebo.launch.py")))
 
 
-
+     
     ## Robot State Publisher
     rob_desc = os.path.join(get_package_share_directory("ah_square_service_cpp"),"model" , "my_tb3.urdf.xacro" )
     robot_state_publihser = Node(package="robot_state_publisher" , executable="robot_state_publisher" , name="robot_state_publisher" , output="screen",
-                                 parameters=[{'robot_description': Command("xacro " , rob_desc)}] #why parameters? see the robot_state_publisher source code in github! It also uses the std::map and I think thats the reason for using key value pair
-                                )
+                                 parameters=[{'robot_description': Command(["xacro " , rob_desc])}] #why parameters? see the robot_state_publisher source code in github! It also uses the std::map and I think thats the reason for using key value pair
+                                ) #why Command needs [] ? see through the Command function and you'll reach union with []!
 
 
     ## Spawn tb3
     spawn_robot = Node(package="gazebo_ros" , executable="spawn_entity.py" , name="spwan_tb3" , output="screen",
-                        arguments=[]
-                        )
+                        arguments=[
+                            '-topic' , 'robot_description' ,
+                            '-entity' , "tb3_"+str(random.random()*10000) , 
+                            "-x" , "0.0" , "-y" , "0.0" , "-z" , "0.0",
+                            "-R" , "0.0" , "-P" , "0.0" , "-Y" , "0.0"
+                        ])
 
     ## My Nodes
+    square_service_server = Node(package="ah_square_service_cpp" , executable="square_server" , name="sqaure_server" , output="screen")
 
 
 
     ld = LaunchDescription()
     ld.add_action(args)
     ld.add_action(gazebo)
+    ld.add_action(robot_state_publihser)
+    ld.add_action(spawn_robot)
+    ld.add_action(square_service_server)
 
     return ld
--- ah_square_service_cpp/src/square_service_server.cpp
+++ ah_square_service_cpp/src/square_service_server.cpp
@@ -1,20 +1,45 @@
 #include<rclcpp/rclcpp.hpp>
-#include<std_srvs/srv/empty.hpp>
+#include<geometry_msgs/msg/twist.hpp>
+#include<ah_square_service_cpp/srv/my_custom_service_message.hpp>
+
+using  CustomReqPtr = ah_square_service_cpp::srv::MyCustomServiceMessage::Request::SharedPtr;
+using  CustomResPtr = ah_square_service_cpp::srv::MyCustomServiceMessage::Response::SharedPtr;
+
 class SquareServer : public rclcpp::Node
 {
     public:
         SquareServer():Node("square_service_server_node")
         {
-            square_service = this->create_service<std_srvs::srv::Empty>("square_server" , std::bind(&SquareServer::serviceServer ,this ,std::placeholders::_1 , std::placeholders::_2));
+            square_service_ = this->create_service<ah_square_service_cpp::srv::MyCustomServiceMessage>("square_server" , std::bind(&SquareServer::serviceServer ,this ,std::placeholders::_1 , std::placeholders::_2));
+            pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel" , 10);
+            vel_lin_.linear.x = 0.1;
+            vel_lin_.angular.z = 0.05;
+            
+            
         }
 
     private:
-        void serviceServer(const std_srvs::srv::Empty::Request::SharedPtr req , const std_srvs::srv::Empty::Response::SharedPtr res)
+        void serviceServer(const CustomReqPtr req , const CustomResPtr res)
         {
             RCLCPP_INFO(this->get_logger() , "Service has been called");
+
+            // double time_needed;
+            // for(int i = 0 ; i< reps ; i++)
+            // {
+            //     for (int j = 0 ; j <4 ; j++)
+            //     {
+            //     }
+            // }
+              
         }
-        rclcpp::Service<std_srvs::srv::Empty>::SharedPtr square_service;
+
+
+        rclcpp::Service<ah_square_service_cpp::srv::MyCustomServiceMessage>::SharedPtr square_service_;
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
         
+        geometry_msgs::msg::Twist vel_lin_ ;
+        geometry_msgs::msg::Twist vel_ang_;
+        geometry_msgs::msg::Twist vel_stop_;
 
 };
 
